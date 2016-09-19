83695d1b3997a7a8472748ab9f70ca0e4d78410d
Katya Demidova <demidovakatya@gmail.com>
2016-09-19 23:44:59 +0300

    added make_text_from_posts

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import re
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -37,11 +38,46 @@ def get_posts_from_thread(url):
     for hit in soup.findAll(attrs={'class' : 'post-message'}):
         this_post = hit.get_text(separator = ' ')
         # remove quotes, unnecessary punctuation, etc
-        this_post = re.sub(r'>>[0-9]*|\(OP\)|>', '', this_post).strip()
+        this_post = re.sub(r'>>[0-9]*|\(OP\)|>', '', this_post)
         this_post = re.sub(r'&gt;', ' ', this_post).strip()
 
         # print(this_post)
         posts.append(this_post)
 
+    print('Found: %s posts' % len(posts))
     return posts
 
+def make_text_from_posts(board = 'b', save_to_txt = True):
+    thread_urls = get_thread_urls(board)
+
+    threads = []
+    for url in thread_urls:
+        threads.append(get_posts_from_thread(url))
+
+    text = '\n'.join(['\n'.join(thread) for thread in threads])
+
+    if save_to_txt:
+        filename = board + '_posts_' + text[:10] + '.txt'
+        with open(filename, 'w') as txt_file:
+            txt_file.write(text)
+        print('Successfully saved bullshit to: %s\n' % filename)
+
+# ============================================================
+
+from pymarkovchain import MarkovChain
+
+# import scraper
+
+text = make_text_from_posts()
+
+def create_mc(text = text):
+    mc = MarkovChain()
+    mc.generateDatabase(text, '\n')
+    return mc
+
+def generate_strings(mc, n_strings = 50):
+    for _ in range(n_strings): 
+        print(mc.generateString())
+
+mc = create_mc()
+generate_strings(mc = mc)
