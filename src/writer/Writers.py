56b8807ba0fd79ade5816d9ce38975e42dffb4ab
Katya Demidova <demidovakatya@gmail.com>
2016-10-25 22:08:40 +0300

    small fix

--- src/writer/Writers.py
+++ src/writer/Writers.py
@@ -3,10 +3,11 @@ from src.writer.TextFileWriter import TextFileWriter
 
 
 class Writers:
+
     def make(self, writer, storage_path):
-        if writer == 'txt':
+        if writer   == 'txt':
             return TextFileWriter(storage_path)
         elif writer == 'console':
             return ConsoleWriter()
         else:
-            raise Exception('Not existing writer: %s!' % writer)
+            raise Exception('Writer not found: %s!' % writer)
