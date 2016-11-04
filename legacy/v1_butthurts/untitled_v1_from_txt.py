65a416f5cde2894c1cb8f4330730b310ee17b05b
Katya Demidova <demidovakatya@gmail.com>
2016-11-04 16:02:22 +0300

    removed legacy (because wtf)

--- legacy/v1_butthurts/untitled_v1_from_txt.py
+++
@@ -1,8 +0,0 @@
-import re
-text = re.sub('\n\n', '\n', open('butthurts.txt').read())
-
-from pymarkovchain import MarkovChain
-mc = MarkovChain(".")
-mc.generateDatabase(text, '\n')
-
-mc.generateString()
